94abdfb83cb0384df291e0669296edac4f739551
Eduard Valeyev <eduard@valeyev.net>
2025-05-20 16:08:24 -0400

    umpire-cxx-allocator is required prerequisite

--- cmake/modules/ExternalDependenciesVersions.cmake
+++ cmake/modules/ExternalDependenciesVersions.cmake
@@ -1,12 +1,13 @@
 # for each dependency track both current and previous id (the variable for the latter must contain PREVIOUS)
 # to be able to auto-update them
 
-set(TTG_TRACKED_VG_CMAKE_KIT_TAG cda539db32be6e8171f5cbebdb1a7c38d5ab4b34)  # provides FindOrFetchLinalgPP and "real" FindOrFetchBoost
+set(TTG_TRACKED_VG_CMAKE_KIT_TAG 4c949fd7ccfe4b4f0e103288a5c0f557c6e740c0)  # provides FindOrFetchLinalgPP and "real" FindOrFetchBoost
 set(TTG_TRACKED_CATCH2_VERSION 3.5.0)
 set(TTG_TRACKED_MADNESS_TAG 71d0936adf013ca96967cb517beae30bd15521d0)
 set(TTG_TRACKED_PARSEC_TAG parsec-for-ttg)
+set(TTG_TRACKED_UMPIRE-CXX-ALLOCATOR_TAG a48ad360e20b9733263768b54aa24afe5894faa4)
 set(TTG_TRACKED_BTAS_TAG c25b0a11d2a76190bfb13fa72f9e9dc3e57c3c2f)
-set(TTG_TRACKED_TILEDARRAY_TAG 42e0d65df9400469d29ebeb929b045be1caaf3d6)
+set(TTG_TRACKED_TILEDARRAY_TAG 136ae0999ca0c750d77a55891b677fc3b2f6e00e)
 
 # need Boost.CallableTraits (header only, part of Boost 1.66 released in Dec 2017) for wrap.h to work
 # BUT if will be building examples, inherit the oldest version from the pickiest Boost consumer (TA and/or BSPMM)
